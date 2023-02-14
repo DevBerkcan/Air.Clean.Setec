f80652c651a9d6c1c99fd237f03124f79b2a8a90
bilalbentoumi <bilalbentoumi@gmail.com>
2023-02-14 16:26:12 +0100

    Refactor layout & create simple hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,91 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import logo from '../assets/images/logo.svg'
+import { useState } from 'react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Dialog } from '@headlessui/react'
+
+const navigation = [
+  { name: 'Home', href: '#' },
+  { name: 'About', href: '#' },
+  { name: 'Services', href: '#' },
+  { name: 'Contact', href: '#' },
+]
 
 const Header: any = () => {
 
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
-    <header className="relative bg-white shadow shadow-gray-200">
+    <header className="px-6 pt-6 lg:px-8">
+      <nav className="flex items-center justify-between" aria-label="Global">
+        <div className="container flex items-center">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">AK</span>
+              <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            </a>
+          </div>
+          <div className="flex-grow lg:hidden"></div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}>
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </nav>
 
-      <div className="container p-6 flex justify-between items-center gap-10">
-
-        <Link href="/">
-          <Image src={logo} alt="Landing Page" height={40} />
-        </Link>
-
-        <ul className="gap-10 hidden sm:flex items-center">
-          <li>
-            <Link href="/">
-              <span className="block text-gray-400 hover:text-gray-500 text-md font-semibold duration-75">Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <span className="block text-gray-400 hover:text-gray-500 text-md font-semibold duration-75">About</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/services">
-              <span className="block text-green-400 hover:text-green-500 text-md font-semibold duration-75">Services</span>
-            </Link>
-          </li>
-          <Link href="/contact">
-            <span className="block px-6 py-2 bg-blue-400 hover:bg-blue-500 text-white text-md font-semibold rounded-md duration-75">Contact</span>
-          </Link>
-        </ul>
-
-      </div>
-
+      <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}>
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   )
 }
