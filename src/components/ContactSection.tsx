2c4206c8569f5e41e10bd1acdcbf93d43ae491d4
bilalbentoumi <bilalbentoumi@gmail.com>
2023-02-15 15:26:03 +0100

    Update contact form

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -5,44 +5,99 @@ const ContactSection: any = () => {
       <div className="container">
 
         <div className="py-8 lg:py-16 mx-auto max-w-screen-md">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">
             Contact Us
           </h2>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">
             Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
           </p>
-          <form action="#" className="space-y-8">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input
-                type="email"
-                id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                placeholder="name@akservices.com"
-                required />
+
+          <form className="space-y-8" action="https://getform.io/f/ade46548-134f-4cd4-8b43-31a274cd5cd1" method="POST">
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900">Company name</label>
+                <input
+                  type="text"
+                  name="company"
+                  id="company"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                  placeholder="AK Services"
+                  required />
+              </div>
+              <div>
+                <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900">Contact person</label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                  placeholder="John Doe"
+                  required />
+              </div>
             </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                  placeholder="name@akservices.com"
+                  required />
+              </div>
+              <div>
+                <label htmlFor="postcode" className="block mb-2 text-sm font-medium text-gray-900">Postal code</label>
+                <input
+                  type="text"
+                  name="postcode"
+                  id="postcode"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                  placeholder="00000"
+                  required />
+              </div>
+            </div>
+
             <div>
-              <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+              <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900">Type of cleaning</label>
               <input
                 type="text"
-                id="subject"
+                name="type"
+                id="type"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                placeholder="Let us know how we can help you"
+                placeholder="Floor cleaning"
                 required />
             </div>
-            <div className="sm:col-span-2">
-              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea
-                id="message"
-                rows={6}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500"
-                placeholder="Leave a comment..."></textarea>
+
+            <div>
+              <label htmlFor="area" className="block mb-2 text-sm font-medium text-gray-900">Cleaning area</label>
+              <input
+                type="text"
+                name="area"
+                id="area"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Area"
+                required />
             </div>
+            
+            <div>
+              <label htmlFor="cycle" className="block mb-2 text-sm font-medium text-gray-900">Cycle</label>
+              <input
+                type="file"
+                name="cycle"
+                id="cycle"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Cycle"
+                required />
+            </div>
+
             <button
               type="submit"
               className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-indigo-700 sm:w-fit hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300">
-              Send message
+              Submit
             </button>
+
           </form>
         </div>
 
