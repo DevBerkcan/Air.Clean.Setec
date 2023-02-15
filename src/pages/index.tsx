d8d215f36ad11d91dd2cafdb62264c9dcb6a6a49
bilalbentoumi <bilalbentoumi@gmail.com>
2023-02-15 14:20:55 +0100

    Aff contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header'
 import HeroSection from '@/components/HeroSection'
 import AboutSection from '@/components/AboutSection'
 import ServicesSection from '@/components/ServicesSection'
+import ContactSection from '@/components/ContactSection'
 import Footer from '@/components/Footer'
 
 export default function Index() {
@@ -52,6 +53,9 @@ export default function Index() {
 
           {/* Services */}
           <ServicesSection />
+
+          {/* Contact */}
+          <ContactSection />
 
           {/* Hero */}
           <Footer />
