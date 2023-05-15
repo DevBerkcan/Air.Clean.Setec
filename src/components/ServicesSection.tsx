26f4bf3a453913a9aef1b244428c67af392042c6
Bilal Bentoumi <bilalbentoumi@gmail.com>
2023-05-15 12:39:39 +0100

    Refactor services section Decrease card description text size

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -5,6 +5,46 @@ import floor from '../assets/images/floor.svg'
 import office from '../assets/images/office.svg'
 import bathroom from '../assets/images/bathroom.svg'
 
+const services = [
+  {
+    title: 'Unterhaltsreinigung',
+    description: "Die Unterhaltsreinigung ist eine regelmäßige Reinigung, die dazu dient, Räumlichkeiten in einem guten und gepflegten Zustand zu halten. Dazu gehört zum Beispiel das Reinigen von Böden, Oberflächen und sanitären Anlagen.",
+    icon: house,
+    color: 'green'
+  },
+  {
+    title: 'Grundreinigung',
+    description: "Die Grundreinigung ist eine intensive Reinigung, die in größeren Abständen durchgeführt wird. Dabei werden Verschmutzungen beseitigt, die bei der Unterhaltsreinigung nicht entfernt werden können. Hierzu zählen zum Beispiel hartnäckige Flecken, Kalkablagerungen oder Fettflecken.",
+    icon: kitchen,
+    color: 'blue'
+  },
+  {
+    title: 'Bauendreinigung',
+    description: "Die Bauendreinigung ist eine Reinigung, die nach Fertigstellung eines Bauvorhabens durchgeführt wird. Dabei werden Baustaub, Bauschutt und weitere Verschmutzungen beseitigt und die Räumlichkeiten auf den Einzug vorbereitet.",
+    icon: floor,
+    color: 'orange'
+  },
+  {
+    title: 'Fensterreinigung',
+    description: "Die Fensterreinigung beinhaltet die Reinigung von Fenstern und Glasflächen innen und außen. Hierbei werden Schmutz, Staub und Fingerabdrücke entfernt, um eine klare Sicht zu gewährleisten.",
+    icon: bathroom,
+    color: 'red'
+  },
+  {
+    title: 'Desinfektionsreinigung',
+    description: "Die Desinfektionsreinigung ist eine Reinigung, die speziell dazu dient, Keime, Bakterien und Viren abzutöten. Diese Art der Reinigung wird in sensiblen Bereichen, wie Praxen, Kindergärten, Altersheimen oder Lebensmittelbetrieben durchgeführt.",
+    icon: office,
+    color: 'indigo'
+  },
+  {
+    title: 'Sonderreinigung',
+    description: "Die Sonderreinigung bezieht sich auf Reinigungsarbeiten, die nicht unter die oben genannten Kategorien fallen. Beispiele für Sonderreinigungen können sein: Teppichreinigung, Fassadenreinigung oder Reinigung von Maschinen und Anlagen.",
+    icon: office,
+    color: 'gray'
+  },
+
+]
+
 const ServicesSection: any = () => {
 
   return (
@@ -16,68 +56,17 @@ const ServicesSection: any = () => {
 
         <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
 
-          <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-green-400">
-              <Image src={house} width={30} alt="house" />
+          {services.map((item, index) => (
+            <div key={index} className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
+              <div className={`w-20 h-20 flex justify-center items-center rounded-full bg-${item.color}-400`}>
+                <Image src={item.icon} width={30} alt={item.title} />
+              </div>
+              <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">{item.title}</h3>
+              <p className="mt-2 text-md leading-[28px] text-gray-600">
+                {item.description}
+              </p>
             </div>
-            <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">Unterhaltsreinigung</h3>
-            <p className="mt-2 text-lg leading-7 text-gray-600">
-            Die Unterhaltsreinigung ist eine regelmäßige Reinigung, die dazu dient, Räumlichkeiten in einem guten und gepflegten Zustand zu halten. 
-            Dazu gehört zum Beispiel das Reinigen von Böden, Oberflächen und sanitären Anlagen.
-            </p>
-          </div>
-          <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-blue-400">
-              <Image src={kitchen} width={30} alt="house" />
-            </div>
-            <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">Grundreinigung</h3>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-            Die Grundreinigung ist eine intensive Reinigung, die in größeren Abständen durchgeführt wird. 
-            Dabei werden Verschmutzungen beseitigt, die bei der Unterhaltsreinigung nicht entfernt werden können. 
-            Hierzu zählen zum Beispiel hartnäckige Flecken, Kalkablagerungen oder Fettflecken.
-            </p>
-          </div>
-          <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-orange-400">
-              <Image src={floor} width={30} alt="house" />
-            </div>
-            <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">Bauendreinigung</h3>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-            Die Bauendreinigung ist eine Reinigung, die nach Fertigstellung eines Bauvorhabens durchgeführt wird. 
-            Dabei werden Baustaub, Bauschutt und weitere Verschmutzungen beseitigt und die Räumlichkeiten auf den Einzug vorbereitet.
-            </p>
-          </div>
-          <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-red-400">ç
-              <Image src={bathroom} width={30} alt="house" />
-            </div>
-            <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">Fensterreinigung</h3>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-            Die Fensterreinigung beinhaltet die Reinigung von Fenstern und Glasflächen innen und außen. Hierbei werden Schmutz, 
-            Staub und Fingerabdrücke entfernt, um eine klare Sicht zu gewährleisten.
-            </p>
-          </div>
-          <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-indigo-400">
-              <Image src={office} width={30} alt="house" />
-            </div>
-            <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">Desinfektionsreinigung</h3>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-            Die Desinfektionsreinigung ist eine Reinigung, die speziell dazu dient, Keime, Bakterien und Viren abzutöten. 
-            Diese Art der Reinigung wird in sensiblen Bereichen, wie Praxen, Kindergärten, Altersheimen oder Lebensmittelbetrieben durchgeführt.
-            </p>
-          </div>
-
-          <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg hover:shadow-xl p-8 duration-100">
-            <div className="w-20 h-20 flex justify-center items-center rounded-full bg-indigo-400">
-              <Image src={office} width={30} alt="house" />
-            </div>
-            <h3 className="mt-6 text-xl font-bold tracking-tight text-gray-900">Sonderreinigung</h3>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-            Die Sonderreinigung bezieht sich auf Reinigungsarbeiten, die nicht unter die oben genannten Kategorien fallen. Beispiele für Sonderreinigungen können sein: 
-            Teppichreinigung, Fassadenreinigung oder Reinigung von Maschinen und Anlagen.
-            </p>
-          </div>
+          ))}
 
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    'bg-green-400',
+    'bg-blue-400',
+    'bg-orange-400',
+    'bg-red-400',
+    'bg-indigo-400',
+    'bg-gray-400',
+  ]
 }
