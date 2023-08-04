c3bd33db5d9c70d370177e7e618ef440f3d3c65d
Bilal Bentoumi <bilalbentoumi@gmail.com>
2023-08-04 14:40:27 +0100

    Fix postcss and linter warnings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
-import useScroll from '@/hooks/useScroll'
 import HeroSection from '@/components/HeroSection'
 import AboutSection from '@/components/AboutSection'
 import ServicesSection from '@/components/ServicesSection'
@@ -10,22 +8,6 @@ import ContactSection from '@/components/ContactSection'
 import Footer from '@/components/Footer'
 
 export default function Index() {
-
-  const { scroll } = useScroll()
-
-  useEffect(() => {
-    let reveals = document.querySelectorAll('.reveal')
-    for (let i = 0; i < reveals.length; i++) {
-      let windowHeight = window.innerHeight
-      let elementTop = reveals[i].getBoundingClientRect().top
-      let elementVisible = 150
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add('active')
-      } else {
-        reveals[i].classList.remove('active')
-      }
-    }
-  }, [scroll])
 
   return (
     <>
--- src/pages/send-offer.tsx
+++ src/pages/send-offer.tsx
@@ -203,7 +203,7 @@ export default function SendOffer() {
                   className="inline-flex py-3 px-5 mt-8 text-sm font-medium text-center text-white rounded-lg bg-indigo-700 sm:w-fit hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 disabled:bg-indigo-400 disabled:cursor-not-allowed"
                   disabled={formik.isSubmitting}>
                   {formik.isSubmitting && <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                   </svg>}
                   Senden
@@ -221,7 +221,7 @@ export default function SendOffer() {
                     <button type="button" onClick={hideAlert} className={"ml-auto -mx-1.5 -my-1.5 p-1.5 rounded-lg focus:ring-2 inline-flex items-center justify-center h-8 w-8 " + (alertType == 'success' ? 'bg-green-50 text-rgreened-500 focus:ring-green-400 hover:bg-green-200' : 'bg-red-50 text-red-500 focus:ring-red-400 p-1.5 hover:bg-red-200')} data-dismiss-target="#alert-2" aria-label="Close">
                       <span className="sr-only">Close</span>
                       <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                       </svg>
                     </button>
                   </div>
