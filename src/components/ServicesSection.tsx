12b8d3747122d6bad7045636f5dabf02f819307f
Bilal Bentoumi <bilalbentoumi@gmail.com>
2023-08-07 19:27:06 +0100

    Add img comparison slider before/after to services section

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import IconLogo from './IconLogo'
-import slide1 from '@/assets/images/pictures/1.jpeg'
+import { ImgComparisonSlider } from '@img-comparison-slider/react'
+import before1 from '@/assets/images/pictures/before.jpeg'
+import before2 from '@/assets/images/pictures/before2.jpeg'
+import after1 from '@/assets/images/pictures/after.jpeg'
+import after2 from '@/assets/images/pictures/after2.jpeg'
 
 const ServicesSection: any = () => {
 
@@ -12,13 +16,18 @@ const ServicesSection: any = () => {
         <span className="mx-auto mb-4 text-sm font-semibold bg-orange-100 text-orange-400 rounded-full px-3 py-1">Services</span>
       </div>
 
-      <div className="container py-8 lg:py-16 space-y-20">
+      <div className="container py-8 lg:py-16 space-y-8 lg:space-y-20">
 
         {/* Services */}
-        <div className="grid lg:grid-cols-2">
-          <Image className="rounded-lg mx-auto mb-4 lg:-rotate-[2deg]" src={slide1} height={400} alt="aaa" />
+        <div className="grid items-start gap-y-6 lg:grid-cols-2">
+          <div className="flex max-w-[500px] rounded-lg mx-auto lg:-rotate-[2deg] overflow-hidden">
+            <ImgComparisonSlider>
+              <Image slot="first" src={before1} alt="Before" />
+              <Image slot="second" src={after1} alt="After" />
+            </ImgComparisonSlider>
+          </div>
           <div>
-            <h2 className="mb-4 text-2xl sm:text-4xl font-bold tracking-tight text-gray-900">Our Services</h2>
+            <h2 className="mb-2 lg:mb-4 text-2xl sm:text-4xl font-bold tracking-tight text-gray-900">Our Services</h2>
             <p className="mb-6 text-md leading-[28px] text-gray-600">
               Unregelmäßig gereinigte Küchenabluftanlagen können nicht nur zu einer schlechten
               Luftzirkulation in Küchen und gastronomischen Einrichtungen führen, sondern auch ein
@@ -29,10 +38,15 @@ const ServicesSection: any = () => {
         </div>
 
         {/* How It Works */}
-        <div className="grid lg:grid-cols-2">
-          <Image className="rounded-lg mx-auto mb-4 lg:order-2 lg:rotate-[2deg]" src={slide1} height={400} alt="aaa" />
+        <div className="grid items-start gap-y-6 lg:grid-cols-2">
+          <div className="flex max-w-[500px] rounded-lg mx-auto lg:order-2 lg:rotate-[2deg] overflow-hidden">
+            <ImgComparisonSlider>
+              <Image slot="first" src={before2} alt="Before" />
+              <Image slot="second" src={after2} alt="After" />
+            </ImgComparisonSlider>
+          </div>
           <div className="lg:order-1">
-            <h2 className="mb-4 text-2xl sm:text-4xl font-bold tracking-tight text-gray-900">How does a cleaning work?</h2>
+            <h2 className="mb-2 lg:mb-4 text-2xl sm:text-4xl font-bold tracking-tight text-gray-900">How does a cleaning work?</h2>
             <p className="mb-6 text-md leading-[28px] text-gray-600">
               Unsere Küchenabluftreinigung erfolgt während des Betriebsstillstands oder nachts. Sie
               müssen nicht permanent vor Ort sein, sondern nur zu Beginn und am Ende für die Abnahme.
@@ -56,10 +70,15 @@ const ServicesSection: any = () => {
         </div>
 
         {/* Legal Situation */}
-        <div className="grid lg:grid-cols-2">
-          <Image className="rounded-lg mx-auto mb-4 lg:-rotate-[2deg]" src={slide1} height={400} alt="aaa" />
+        <div className="grid items-start gap-y-6 lg:grid-cols-2">
+          <div className="flex max-w-[500px] rounded-lg mx-auto lg:-rotate-[2deg] overflow-hidden">
+            <ImgComparisonSlider>
+              <Image slot="first" src={before1} alt="Before" />
+              <Image slot="second" src={after1} alt="After" />
+            </ImgComparisonSlider>
+          </div>
           <div>
-            <h2 className="mb-4 text-2xl sm:text-4xl font-bold tracking-tight text-gray-900">Rechtslage zur Küchenabluftreinigung</h2>
+            <h2 className="mb-2 lg:mb-4 text-2xl sm:text-4xl font-bold tracking-tight text-gray-900">Rechtslage zur Küchenabluftreinigung</h2>
             <p className="mb-6 text-md leading-[28px] text-gray-600">
               Die rechtlichen Vorgaben zur Reinigung von gewerblichen Küchenabluftanlagen sind durch die VDI Richtlinie 2052 in Verbindung mit der BGR 111 und dem Arbeitsschutzgesetz (ArbSchG) festgelegt. Gemäß diesen Vorschriften sind regelmäßige Inspektionen und eine fachgerechte Reinigung der Anlagen erforderlich. Wenn diese Vorgaben nicht oder unzureichend beachtet werden, kann es im Falle von Schäden oder Beanstandungen zu einem sogenannten Organisationsverschulden kommen. In solchen Fällen haften sowohl die Unternehmensführung als auch der verantwortliche Mitarbeiter.
             </p>
